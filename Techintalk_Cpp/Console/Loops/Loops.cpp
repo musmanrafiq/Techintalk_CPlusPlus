34600bb01c121dd8e9de1af0b0a94f8efd2b1a06
usman_solemn <usman.solemn@gmail.com>
2021-12-15 11:07:01 +0500

    updates

--- Techintalk_Cpp/Console/Loops/Loops.cpp
+++ Techintalk_Cpp/Console/Loops/Loops.cpp
@@ -3,11 +3,14 @@
 int main()
 {
 	// for loop
+	std::cout << "This is for loop" << "\n";
+
 	for (int iteration = 0; iteration < 5; iteration++) {
 		std::cout << "Iteration # " << iteration << "\n";
 	}
 
 	// while loop
+	std::cout << "This is while loop" << "\n";
 	int count = 0;
 	while(count < 5) {
 		std::cout << "Iteration # " << count << "\n";
