743b2ae38bc890020ced12fb379ea74aa8fe4e56
usman_solemn <usman.solemn@gmail.com>
2021-12-15 10:53:32 +0500

    For loop

--- Techintalk_Cpp/Console/Loops/Loops.cpp
+++ Techintalk_Cpp/Console/Loops/Loops.cpp
@@ -1,31 +1,9 @@
 #include <iostream>
-using std::cout;
-
 
 int main()
 {
-    bool shouldRepeat = false;
-
-    for (int i = 0; i < 5; i++) {
-       // cout << "Hello World!\n";
-    }
-
-    int count = 0;
-
-    while (shouldRepeat) {
-       cout << "Hello World!\n";
-       //count++;
-    }
-        
-    do {
-        cout << "Hello World!\n";
-        //count++;
-    } while (shouldRepeat);
-}
-
-void ForLoop() {
-    for (int i = 0; i < 5; i++) {
-        cout << "Hello World!\n";
-    }
+	for (int count = 0; count < 5; count++) {
+		std::cout << "Iteration # " << count << "\n";
+	}
 }
 
