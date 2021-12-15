2095b952d9f537ad5b9994fe53ae63a23b002382
usman_solemn <usman.solemn@gmail.com>
2021-12-15 11:05:38 +0500

    updates, added while loop as well

--- Techintalk_Cpp/Console/Loops/Loops.cpp
+++ Techintalk_Cpp/Console/Loops/Loops.cpp
@@ -2,8 +2,16 @@
 
 int main()
 {
-	for (int count = 0; count < 5; count++) {
+	// for loop
+	for (int iteration = 0; iteration < 5; iteration++) {
+		std::cout << "Iteration # " << iteration << "\n";
+	}
+
+	// while loop
+	int count = 0;
+	while(count < 5) {
 		std::cout << "Iteration # " << count << "\n";
+		count += 1;
 	}
 }
 
